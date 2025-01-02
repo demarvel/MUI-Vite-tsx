bacace197622b7241862d77cfaa63ea0dcbf07ee
s8076226 <s8076226@gmail.com>
2025-01-02 14:56:46 +0400

    ok

--- src/Components/Tabs/Icon position.tsx
+++ src/Components/Tabs/Icon position.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
-import Tabs from "@mui/material/Tabs";
+//import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { Box } from "@mui/material";
 import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
 import LoginIcon from "@mui/icons-material/Login";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import BasicStack from "../../log in/New Blocks/Card Content";
+//import BasicStack from "../../log in/New Blocks/Card Content";
 
 export default function IconPositionTabs() {
   const [value, setValue] = React.useState(0);
@@ -12,16 +15,51 @@ export default function IconPositionTabs() {
     setValue(newValue);
   };
 
+  
+
+  //   return (
+  //     <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+  //       <Tabs value={value} onChange={handleChange} centered variant="fullWidth">
+  //         <Tab label="вход" icon={<LoginIcon />} iconPosition="start"></Tab>
+
+  //         <Tab
+  //           label="регистрация"
+  //           icon={<AppRegistrationIcon />}
+  //           iconPosition="start"
+  //         />
+  //       </Tabs>
+  //     </Box>
+
+  //   );
+  // }
+
   return (
     <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-      <Tabs value={value} onChange={handleChange} centered variant="fullWidth">
-        <Tab label="вход" icon={<LoginIcon />} iconPosition="start" />
-        <Tab
-          label="регистрация"
-          icon={<AppRegistrationIcon />}
-          iconPosition="start"
-        />
-      </Tabs>
+      <TabContext
+        value={value}
+        // onChange={handleChange}
+        // centered
+        // variant="fullWidth"
+      >
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList centered variant="fullWidth" onChange={handleChange} aria-label="lab API tabs example">
+            <Tab
+              label="вход"
+              icon={<LoginIcon />}
+              iconPosition="start"
+              value="1"
+            />
+            <Tab
+              label="регистрация"
+              icon={<AppRegistrationIcon />}
+              iconPosition="start"
+              value="2"
+            />
+          </TabList>
+        </Box>
+        <TabPanel value="1"><BasicStack></BasicStack></TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+      </TabContext>
     </Box>
   );
 }
--- src/log in/Login main.tsx
+++ src/log in/Login main.tsx
@@ -1,12 +1,11 @@
 //import BasicTextFields from "../Components/Autocomplete";
 import IconPositionTabs from "../Components/Tabs/Icon position";
-import BoxBasic from "./New Blocks/Card Content";
+//import BoxBasic from "./New Blocks/Card Content";
 
 const App = () => {
   return (
     <>
       <IconPositionTabs />
-      <BoxBasic />
     </>
   );
 };
