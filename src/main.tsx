a95ab41f90ec6c5b1fffb6494694592f658c1415
s8076226 <s8076226@gmail.com>
2025-01-13 23:19:16 +0400

    23:18 13/01

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { HingeSmallPattern } from "./Components/Test/Test_Konva";
-import App from "./Components/Test/111";
+import MainCircle from "./Section4/MainCircle";
 //import MainLogin from "./Section1/MainLogin";
 //import MainPage from "./Section2/MainPage";
 //import MainOrder from "./Section3/MainOrder";
@@ -13,6 +12,6 @@ createRoot(document.getElementById("root")!).render(
     {/* <MainLogin /> */}
     {/* <MainPage /> */}
     {/* <MainOrder/> */}
-    <App />
+    <MainCircle/>
   </>
 );
